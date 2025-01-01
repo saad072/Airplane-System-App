cc865e7d4f50d8f058953db745a306218488fae2
saad072 <saadasif5678@gmail.com>
2025-01-01 11:19:27 +0500

    Updated

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Saad and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Airplane Ticket', {
+    before_submit: function(frm) {
+        // Check if the status is 'Boarded'
+        if (frm.doc.status !== 'Boarded') {
+            // Prevent submission and show an error message
+            frappe.throw(__('You cannot submit this ticket because the status is not "Boarded".'));
+        }
+    }
+});
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,10 +1,34 @@
-# Copyright (c) 2024, Saad and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
-
-
 class AirplaneTicket(Document):
-	pass
+    def before_save(self):
+        # Ensure each add-on is unique in the add_ons child table
+        self.remove_duplicate_add_ons()
+        # Calculate total add-ons amount
+        total_add_ons = 0.0
+        for add_on in self.add_ons:
+            total_add_ons += add_on.amount or 0
+        # Calculate total amount (flight price + add-ons)
+        self.total_amount = total_add_ons + (self.flight_price or 0)
+        # Ensure only boarded tickets can be updated
+        if not self.status == "Boarded":
+            frappe.throw ("Only boarded tickets can be updated.")
+        else:
+            pass
+    def remove_duplicate_add_ons(self):
+        """Remove duplicate add-ons from the child table."""
+        unique_add_ons = {}
+        cleaned_add_ons = []
+        for add_on in self.add_ons:
+            # Use the item field as the unique identifier
+            if add_on.item not in unique_add_ons:
+                unique_add_ons[add_on.item] = True
+                cleaned_add_ons.append(add_on)
+        # Replace the add_ons child table with the cleaned list
+        self.add_ons = cleaned_add_ons
+
+
+
+
+
 
